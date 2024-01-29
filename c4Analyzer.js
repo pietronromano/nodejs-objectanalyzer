438ce67021868202f30a2cb8ccfd478780cdfcb9
Pietro N. Romano <pietronromano@live.com>
2024-01-29 23:13:06 +0100

    parent-child

--- c4Analyzer.js
+++ c4Analyzer.js
@@ -10,16 +10,17 @@ const COLUMN_SEPARATOR = ",";
  * @param {*} sort 
  */
 const writeToFile = (outputArray,outputFilePath) =>{
-    var content = "Id,Diagram,c4Application,c4Name,c4Description,c4Container,c4Technology,c4Type\n";
+    var content = "Id,Diagram,c4Application,c4Name,c4Container,c4Parent,c4Technology,c4Type,c4Description\n";
     outputArray.forEach(element => {
       content = content + element.id + COLUMN_SEPARATOR  
-                        + element.diagram + COLUMN_SEPARATOR
-                        + utils.ensureValidColValue(element.c4Application) + COLUMN_SEPARATOR
-                        + utils.ensureValidColValue(element.c4Name) + COLUMN_SEPARATOR
-                        + utils.ensureValidColValue(element.c4Container) + COLUMN_SEPARATOR
-                        + utils.ensureValidColValue(element.c4Technology) + COLUMN_SEPARATOR
-                        + utils.ensureValidColValue(element.c4Type) + COLUMN_SEPARATOR 
-                        + utils.ensureValidColValue(element.c4Description) + "\n"
+            + element.diagram + COLUMN_SEPARATOR
+            + utils.ensureValidColValue(element.c4Application) + COLUMN_SEPARATOR
+            + utils.ensureValidColValue(element.c4Name) + COLUMN_SEPARATOR
+            + utils.ensureValidColValue(element.c4Container) + COLUMN_SEPARATOR
+            + utils.ensureValidColValue(element.c4Parent) + COLUMN_SEPARATOR
+            + utils.ensureValidColValue(element.c4Technology) + COLUMN_SEPARATOR
+            + utils.ensureValidColValue(element.c4Type) + COLUMN_SEPARATOR 
+            + utils.ensureValidColValue(element.c4Description) + "\n"
     });
   
     try {
@@ -49,13 +50,23 @@ const analyzeProperties = (outputArray,obj)  =>{
                     var c4Element = {
                         diagram:  d.$.name,
                         id: e.$.id,
+                        parentId: "",
+                        parentElement: undefined,
+                        childElements: [],
                         c4Name: e.$.c4Name, 
                         c4Application: e.$.c4Application, 
                         c4Container: e.$.c4Container,
+                        c4Parent: e.$.c4Parent,
                         c4Description: e.$.c4Description, 
                         c4Technology: e.$.c4Technology, 
                         c4Type: e.$.c4Type 
                     };
+
+                    var geometry = e.mxCell[0].mxGeometry[0].$;
+                    c4Element.x = parseInt(geometry.x); 
+                    c4Element.y= parseInt(geometry.y);  
+                    c4Element.width= parseInt(geometry.width);  
+                    c4Element.height= parseInt(geometry.height); 
                     outputArray.push(c4Element);
                 }
 
@@ -63,11 +74,35 @@ const analyzeProperties = (outputArray,obj)  =>{
         }
    });
 
+   //Links the parents
+   linkParents(outputArray);
 }
 
+const linkParents = (outputArray) =>{
+    outputArray.forEach(element => {
+        for (let i = 0; i < outputArray.length; i++) {
+            const child = outputArray[i];
+            isEl2InsideEl1(element,child);
+        }
+    });
+      
+}
+
+const isEl2InsideEl1 = (el1, el2) => {
+    if ((el1.x < el2.x) && ((el1.x + el1.width) > el2.x )) {
+        if ((el1.y < el2.y) && ((el1.y + el1.height) > el2.y )) {
+            el2.parentElement = el1;
+            el1.childElements.push(el2);
+            if ((el1.c4Name && el2.c4Name))
+                console.log(el1.c4Name + " is parent of " + el2.c4Name)
+            return true;
+        }
+    }
+}
 	 
  module.exports = {
     analyzeProperties: analyzeProperties,
+    linkParents: linkParents,
     writeToFile : writeToFile
 
  }
