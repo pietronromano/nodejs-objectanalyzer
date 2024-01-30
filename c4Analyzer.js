856cbbbb5e2676967224da856bb4c539126f9c5b
Pietro N. Romano <pietronromano@live.com>
2024-01-30 08:52:52 +0100

    parent path

--- c4Analyzer.js
+++ c4Analyzer.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const utils = require('./utils');
 
 const COLUMN_SEPARATOR = ",";
+const PATH_SEPARATOR = "/";
 
 /**
  * 
@@ -10,7 +11,7 @@ const COLUMN_SEPARATOR = ",";
  * @param {*} sort 
  */
 const writeToFile = (outputArray,outputFilePath) =>{
-    var content = "Id,Diagram,c4Application,c4Name,c4Container,c4Parent,c4Technology,c4Type,c4Description\n";
+    var content = "Id,Diagram,c4Application,c4Name,c4Container,c4Parent,c4Path,c4Technology,c4Type,c4Description\n";
     outputArray.forEach(element => {
       content = content + element.id + COLUMN_SEPARATOR  
             + element.diagram + COLUMN_SEPARATOR
@@ -18,6 +19,7 @@ const writeToFile = (outputArray,outputFilePath) =>{
             + utils.ensureValidColValue(element.c4Name) + COLUMN_SEPARATOR
             + utils.ensureValidColValue(element.c4Container) + COLUMN_SEPARATOR
             + utils.ensureValidColValue(element.c4Parent) + COLUMN_SEPARATOR
+            + utils.ensureValidColValue(element.c4Path) + COLUMN_SEPARATOR
             + utils.ensureValidColValue(element.c4Technology) + COLUMN_SEPARATOR
             + utils.ensureValidColValue(element.c4Type) + COLUMN_SEPARATOR 
             + utils.ensureValidColValue(element.c4Description) + "\n"
@@ -52,15 +54,20 @@ const analyzeProperties = (outputArray,obj)  =>{
                         id: e.$.id,
                         parentId: "",
                         parentElement: undefined,
-                        childElements: [],
                         c4Name: e.$.c4Name, 
                         c4Application: e.$.c4Application, 
                         c4Container: e.$.c4Container,
                         c4Parent: e.$.c4Parent,
+                        c4Path: e.$.c4Path,
                         c4Description: e.$.c4Description, 
                         c4Technology: e.$.c4Technology, 
                         c4Type: e.$.c4Type 
                     };
+
+                    //Try to ensure a name
+                    if(!c4Element.c4Name) c4Element.c4Name = "NAME MISSING! Type:" + c4Element.c4Type
+                    if(!c4Element.c4Name) c4Element.c4Name = "NAME MISSING! Technology:" + c4Element.c4Technology
+                    c4Element.c4Path = PATH_SEPARATOR + c4Element.c4Name
 
                     var geometry = e.mxCell[0].mxGeometry[0].$;
                     c4Element.x = parseInt(geometry.x); 
@@ -82,19 +89,44 @@ const linkParents = (outputArray) =>{
     outputArray.forEach(element => {
         for (let i = 0; i < outputArray.length; i++) {
             const child = outputArray[i];
-            isEl2InsideEl1(element,child);
+            isInside(element,child);
         }
     });
       
+    //Set paths after parents
+    outputArray.forEach(element => {
+        let parents = [];
+        parent = element.parentElement;
+        while (parent) {
+            parents.push(parent);
+            parent = parent.parentElement;
+        }
+        parents.reverse();
+        parents.forEach(parent => {
+            element.c4Path = parent.c4Path + PATH_SEPARATOR + element.c4Name;
+        });
+        console.log(element.c4Path);
+    });
+    
+
 }
 
-const isEl2InsideEl1 = (el1, el2) => {
-    if ((el1.x < el2.x) && ((el1.x + el1.width) > el2.x )) {
-        if ((el1.y < el2.y) && ((el1.y + el1.height) > el2.y )) {
-            el2.parentElement = el1;
-            el1.childElements.push(el2);
-            if ((el1.c4Name && el2.c4Name))
-                console.log(el1.c4Name + " is parent of " + el2.c4Name)
+const isInside = (outer, inner) => {
+    if (outer.diagram != inner.diagram)
+        return false; // remember there will be multiple diagrmas
+
+    if ((outer.x < inner.x) && ((outer.x + outer.width) > inner.x )) {
+        if ((outer.y < inner.y) && ((outer.y + outer.height) > inner.y )) {
+            //Check if existing parent is inside this new outer element
+            if(inner.parentElement && isInside(outer,inner.parentElement))
+            {
+                return false;
+            }
+            else {
+                inner.c4Parent = outer.c4Name;
+                inner.parentElement = outer;
+                console.log(outer.c4Name + " is parent of " + inner.c4Name);
+            }
             return true;
         }
     }
